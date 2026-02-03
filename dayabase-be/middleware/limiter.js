86c1808613fdee58ca03dbcdaa86dff5c6477c9f
Diardo Marendi Krista <diardomarendikrista@gmail.com>
2026-02-03 14:28:51 +0700

    feat(limiter): increase limit per IP

--- dayabase-be/middleware/limiter.js
+++ dayabase-be/middleware/limiter.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
   windowMs: 5 * 60 * 1000, // 5 minutes
-  max: 5000, // Limit each IP to 5000 requests per `window` (here, per 15 minutes)
+  max: 10000, // Limit each IP to 5000 requests per `window`
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message:
